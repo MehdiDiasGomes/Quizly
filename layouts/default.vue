9dd654e875d97812e8666449087e45485d107fa0
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2024-12-02 11:51:34 +0100

    🎉 initialize: quizly

---
+++ layouts/default.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
+import { Input } from '@/components/ui/input'
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
+import { Menu, Package2, Search, CircleUser } from 'lucide-vue-next'
+</script>
+
+<template>
+  <div class="flex min-h-screen w-full flex-col">
+    <header class="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+      <nav class="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+        <!-- <a
+          href="#"
+          class="flex items-center gap-2 text-lg font-semibold md:text-base"
+        >
+          <Package2 class="h-6 w-6" />
+          <span class="sr-only">Acme Inc</span>
+        </a> -->
+        <a
+          href="#"
+          class="text-foreground transition-colors hover:text-foreground"
+        >
+          Accueil
+        </a>
+        <a
+          href="#"
+          class="text-muted-foreground transition-colors hover:text-foreground"
+        >
+          Quiz
+        </a>
+      </nav>
+      <Sheet>
+        <SheetTrigger as-child>
+          <Button
+            variant="outline"
+            size="icon"
+            class="shrink-0 md:hidden"
+          >
+            <Menu class="h-5 w-5" />
+            <span class="sr-only">Toggle navigation menu</span>
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left">
+          <nav class="grid gap-6 text-lg font-medium">
+            <a href="#" class="hover:text-foreground">
+              Accueil
+            </a>
+            <a
+              href="#"
+              class="text-muted-foreground hover:text-foreground"
+            >
+              Quiz
+            </a>
+          </nav>
+        </SheetContent>
+      </Sheet>
+      <div class="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
+        <form class="ml-auto flex-1 sm:flex-initial">
+        </form>
+        <DropdownMenu>
+          <DropdownMenuTrigger as-child>
+            <Button variant="secondary" size="icon" class="rounded-full">
+              <CircleUser class="h-5 w-5" />
+              <span class="sr-only">Toggle user menu</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Settings</DropdownMenuItem>
+            <DropdownMenuItem>Support</DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Logout</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </header>
+  </div>
+</template>
